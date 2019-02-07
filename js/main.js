73b40a6702945bfc4d9ba70bfef7c948b893e2c8
fedebyes <fedeb703@gmail.com>
2019-02-08 00:31:49 +0100

    added stuff

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+window.addEventListener( "resize", function(){
+	var width= window.innerWidth;
+	var height= window.innerHeight;
+	renderer.setSize( width, height);
+	camera.aspect= width/height;
+	camera.updateProjectionMatrix();
+	
+
+});
+
+//shape
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe:true } );
+
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 3;
+var animate = function () {
+	requestAnimationFrame( animate );
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.005;
+  renderer.render( scene, camera );
+			};
+
+
+
+			animate();
